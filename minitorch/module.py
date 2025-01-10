8d42a1a87a479fe17440e0e47dca6145a6926656
tourank <touranm@gmail.com>
2025-01-10 06:46:28 -0500

    implement train() and eval()

--- minitorch/module.py
+++ minitorch/module.py
@@ -31,13 +31,19 @@ class Module:
 
     def train(self) -> None:
         """Set the mode of this module and all descendent modules to `train`."""
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        self.training = True
+
+        #result = map(self.train, self.modules())
+        for module in self.modules():
+            module.train()
 
     def eval(self) -> None:
         """Set the mode of this module and all descendent modules to `eval`."""
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        self.training = False
+
+        for module in self.modules():
+            module.eval()
+
 
     def named_parameters(self) -> Sequence[Tuple[str, Parameter]]:
         """Collect all the parameters of this module and its descendents.
